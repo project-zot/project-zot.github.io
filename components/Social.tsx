b4e27342d376c5632424252dee01b108a4d24450
Raul Kele <raulkeleblk@gmail.com>
2022-09-16 18:59:01 +0300

    removed unused assets (#3)
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import ExportedImage from 'next-image-export-optimizer'
 
 const Social = () => {
     return (
@@ -7,17 +6,17 @@ const Social = () => {
             <ul>
                 <li>
                     <Link href='https://slack.com/'>
-                        <a><ExportedImage src={'images/slack.svg'} alt="Slack Logo" width={40} height={40} /></a>
+                        {/* <a><ExportedImage  alt="Slack Logo" width={40} height={40} /></a> */}
                     </Link>
                 </li>
                 <li>
                     <Link href='https://twitter.com/Cisco/'>
-                        <a><ExportedImage src={'images/twitter_new.svg'} alt="Twitter Logo" width={49} height={40} /></a>
+                        {/* <a><ExportedImage  alt="Twitter Logo" width={49} height={40} /></a> */}
                     </Link>
                 </li>
                 <li>
                     <Link href='https://github.com/'>
-                        <a><ExportedImage src={'images/github.svg'} alt="Github Logo" width={41} height={40} /></a>
+                        {/* <a><ExportedImage alt="Github Logo" width={41} height={40} /></a> */}
                     </Link>
                 </li>
             </ul>
