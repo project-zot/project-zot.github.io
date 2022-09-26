d9f296a38c2c7e7f99588575503252b0ae94482e
Raul Kele <raulkeleblk@gmail.com>
2022-09-26 21:02:26 +0300

    responsiveness work
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,46 +1,102 @@
-import React, { useEffect, useRef } from 'react'
-import ExportedImage from 'next-image-export-optimizer'
-import styles from '@/styles/HeroSection.module.scss'
-import { Button, Stack, Typography } from '@mui/material'
-import Header from './Header'
-import remToPx from 'utils/imageSizeConverter'
-
+import React, { useEffect, useRef } from "react";
+import ExportedImage from "next-image-export-optimizer";
+import styles from "@/styles/HeroSection.module.scss";
+import {
+  Button,
+  createTheme,
+  Grid,
+  Container,
+  Typography,
+  Stack,
+} from "@mui/material";
+import Header from "./Header";
+import remToPx from "utils/imageSizeConverter";
 
 const HeroSection = () => {
-    const zuiHomeSize = useRef({w:806,h:672});
+  const zuiHomeSize = useRef({ w: 806, h: 672 });
 
-    useEffect(() => {
-        console.log('getting size');
-        zuiHomeSize.current = {w:remToPx(42),h:remToPx(50.375)};
-        console.log('size after calc', zuiHomeSize);
-    },[]);
+  // for responsive font
+  const theme = createTheme();
 
-    return (
-        <>
-            <Stack className={styles.heroWrapper} direction='column'>
-                <Header/>
-                <div className={styles.heroImage}>
-                    <ExportedImage src={'images/Hero-zui-preview.png'} height={zuiHomeSize.current.h} width={zuiHomeSize.current.w} alt="zui-home" />
-                </div>
+  theme.typography.h3 = {
+    fontSize: "2.5rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2.0rem",
+    },
+    [theme.breakpoints.up("sm")]: {
+        fontSize: "1.5rem",
+      },
+  };
 
-                <Stack className={styles.heroInteraction} direction='column'>
-                    <Typography className={styles.heroText}>A production-ready vendor-neutral OCI image registry server purely based on OCI standards.</Typography>
-                    <Stack className={styles.buttonContainer} direction='row' spacing={4}>
-                        <Button disableRipple disableFocusRipple size='large' variant='contained' className={styles.getStartedButton}>
-                            <a className={styles.buttonLink} href='http://zotregistry.io/docs-zot/zot-docs-1/1.0/install-guides/install-guide-linux.html'>
-                            Get started
-                            </a>
-                        </Button>
-                        <Button disableRipple  disableFocusRipple variant='outlined' className={styles.learnMoreButton}>
-                            <a className={styles.buttonLink} href='http://zotregistry.io/docs-zot/zot-docs-1/1.0/general/zot-concepts.html'>
-                            Learn more
-                            </a>        
-                        </Button>
-                    </Stack>
-                </Stack>
+  useEffect(() => {
+    console.log("getting size");
+    zuiHomeSize.current = { w: remToPx(42), h: remToPx(50.375) };
+    console.log("size after calc", zuiHomeSize);
+  }, []);
+
+  return (
+    <Grid className={styles.heroWrapper}>
+      <Header />
+
+      <Grid container spacing={3} className={styles.heroContentWrapper}>
+        <Grid container item xs={4} md={4} className={styles.heroInteraction}>
+          <Grid item>
+            <Typography className={styles.heroText}>
+              A production-ready vendor-neutral OCI image registry server purely
+              based on OCI standards.
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            container
+            className={styles.buttonContainer}
+          >
+            <Stack direction="row" spacing={3}>
+              <Button
+                disableRipple
+                disableFocusRipple
+                size="large"
+                variant="contained"
+                className={styles.getStartedButton}
+              >
+                <a
+                  className={styles.buttonLink}
+                  href="http://zotregistry.io/docs-zot/zot-docs-1/1.0/install-guides/install-guide-linux.html"
+                >
+                  Get started
+                </a>
+              </Button>
+              <Button
+                disableRipple
+                disableFocusRipple
+                variant="outlined"
+                className={styles.learnMoreButton}
+              >
+                <a
+                  className={styles.buttonLink}
+                  href="http://zotregistry.io/docs-zot/zot-docs-1/1.0/general/zot-concepts.html"
+                >
+                  Learn more
+                </a>
+              </Button>
             </Stack>
-        </>
-    )
-}
+          </Grid>
+        </Grid>
 
-export default HeroSection
+        <Grid item xs={4} md={4} className={styles.heroImage}>
+          <ExportedImage
+            src={"images/Hero-zui-preview.png"}
+            height={zuiHomeSize.current.h}
+            width={zuiHomeSize.current.w}
+            alt="zui-home"
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HeroSection;
--- components/ProductSection.tsx
+++ components/ProductSection.tsx
@@ -1,54 +1,65 @@
 import React, { useEffect, useRef } from "react";
 import ExportedImage from "next-image-export-optimizer";
 import styles from "@/styles/ProductSection.module.scss";
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Grid, Stack, Typography } from "@mui/material";
 import remToPx from "utils/imageSizeConverter";
 
 const ProductSection = () => {
-  const productZuiPreviewSize = useRef({w:504,h:385});
+  const productZuiPreviewSize = useRef({ w: 504, h: 385 });
 
   useEffect(() => {
-    productZuiPreviewSize.current = {w:remToPx(31.5), h:remToPx(24.063)}
-  },[]);
+    productZuiPreviewSize.current = { w: remToPx(31.5), h: remToPx(24.063) };
+  }, []);
 
   return (
-    <>
-      <div style={{position:'relative'}}>
-        <div className={styles.productImage}>
-          <ExportedImage
-            src={"images/Product-zui-preview.png"}
-            height={productZuiPreviewSize.current.h}
-            width={productZuiPreviewSize.current.w}
-            alt="zui-explore"
-          />
-        </div>
-      </div>
-      <Stack direction="column" className={styles.productContainer} spacing={4}>
-        <Typography className={styles.productTitle}>What is zot?</Typography>
-        <Typography className={styles.productTextBlock}>
-          There are two broad trends occurring currently in how we build,
-          distribute and consume software. The first one is the increasing
-          adoption of container technologies. The second one is that we no
-          longer build everything from scratch but compose our software
-          solutions from various sources but, this also raises the important
-          question of software provenance and supply chain security.
-        </Typography>
-        <Typography className={styles.productTextBlock}>
-          Zot intends to play an important role in this regard. At its heart,
-          zot is a production-ready vendor-neutral OCI image registry with
-          images stored in OCI image format, distribution specification
-          on-the-wire, that&apos;s it!
-        </Typography>
-        <Stack direction="row" spacing={3} className={styles.buttonContainer}>
-          <Button variant="contained" className={styles.howItWorksButton}>
-            How it works
-          </Button>
-          <Button variant="outlined" className={styles.specsLink}>
-            More details
-          </Button>
-        </Stack>
-      </Stack>
-    </>
+    <Grid container spacing={2} className={styles.productContainer}>
+      <Grid
+        item
+        xs={4}
+        md={4}
+        className={styles.productImage}
+      >
+        <ExportedImage
+          src={"images/Product-zui-preview.png"}
+          height={productZuiPreviewSize.current.h}
+          width={productZuiPreviewSize.current.w}
+          alt="zui-explore"
+        />
+      </Grid>
+      <Grid container item xs={4} md={5} spacing={4}>
+        <Grid item xs={12}>
+          <Typography className={styles.productTitle}>What is zot?</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography className={styles.productTextBlock}>
+            There are two broad trends occurring currently in how we build,
+            distribute and consume software. The first one is the increasing
+            adoption of container technologies. The second one is that we no
+            longer build everything from scratch but compose our software
+            solutions from various sources but, this also raises the important
+            question of software provenance and supply chain security.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography className={styles.productTextBlock}>
+            Zot intends to play an important role in this regard. At its heart,
+            zot is a production-ready vendor-neutral OCI image registry with
+            images stored in OCI image format, distribution specification
+            on-the-wire, that&apos;s it!
+          </Typography>
+        </Grid>
+        <Grid item xs={12} className={styles.buttonContainer}>
+          <Stack direction="row" spacing={3} >
+            <Button variant="contained" className={styles.howItWorksButton}>
+              How it works
+            </Button>
+            <Button variant="outlined" className={styles.specsLink}>
+              More details
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
