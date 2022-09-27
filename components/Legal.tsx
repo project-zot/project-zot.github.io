5a2767ec0121c63c3e8174573ad525cc509e2201
Amelia-Maria Breda <ameliamaria.breda@dxc.com>
2022-09-27 15:15:04 +0300

    Updated the link for License and changed Copyright text
    Signed-off-by: Amelia-Maria Breda <ameliamaria.breda@dxc.com>

--- components/Legal.tsx
+++ components/Legal.tsx
@@ -6,13 +6,17 @@ const Legal = () => {
         <div className={styles.legal}>
             <ul>
                 <li>
-                    <Button variant="text" disableRipple className={styles.legalButton}>License rights</Button>
+                    <Button variant="text" disableRipple className={styles.legalButton}>
+                        <a className={styles.legalButton} href='https://github.com/project-zot/zot/blob/main/LICENSE'>
+                            License rights
+                        </a>
+                    </Button>
                 </li>
                 <li>
                     <Button  variant="text" disableRipple className={styles.legalButton}>Privacy Policy</Button>
                 </li>
                 <li>
-                    <Button  variant="text" disableRipple className={styles.legalButton}>Copyright</Button>
+                    <Button  variant="text" disableRipple className={styles.legalButton}>Copyright @2022 Cisco Systmes</Button>
                 </li>
             </ul>
         </div>
