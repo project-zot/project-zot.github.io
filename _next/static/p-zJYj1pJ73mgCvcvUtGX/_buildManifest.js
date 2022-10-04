fe9394ec18a6df2739b4436eb62713f3f4a1b7b5
andaaron <andreifdaaron@gmail.com>
2022-10-04 10:40:39 +0000

    Deploying to gh-pages from @ project-zot/project-zot.github.io@7d28dd4f0dc3b69a7db6498da1dd1e9a7e80441a 🚀

--- _next/static/p-zJYj1pJ73mgCvcvUtGX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-f3202e7320809c10.js","static/css/9a3d0237384596e3.css","static/chunks/pages/index-d96daa708ee8f6d2.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
