4f15e7fd9976f5bccdcfa50d210d8a0468725e21
raulkele <raulkeleblk@gmail.com>
2022-10-24 07:30:32 +0000

    Deploying to gh-pages from @ project-zot/project-zot.github.io@31228ad41afdf4aca6e7bc67b93ddcae63089b26 🚀

--- _next/static/7H2kwrp4jM3Fc54P8K9JX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-f3202e7320809c10.js","static/css/9a3d0237384596e3.css","static/chunks/pages/index-f4c940a7c7f0f74b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
