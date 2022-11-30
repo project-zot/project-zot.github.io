8e25efb0449944644a889e5dd0d3e7e6af4ad324
Ramkumar Chinchani <rchincha@cisco.com>
2022-11-30 19:26:03 +0000

    delete older files

--- utils/imageSizeConverter.js
+++
@@ -1,7 +0,0 @@
-const remToPx = (sizeInRem) => {
-  if(typeof window === "undefined") 
-    return sizeInRem;
-  return  sizeInRem * parseFloat(getComputedStyle(document.documentElement).fontSize);
-}
-
-export default remToPx;
